8014d9307c3cf99df1d0a408244e17dd1f990f23
CYoLee <CYoLee@users.noreply.github.com>
2025-04-10 08:22:32 +0800

    Modify main.py .gitignore

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from redeem_bot import start_discord_bot
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "✅ GuaGuaBOT 正常啟動中"
 
+
 def run_flask():
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
 
 if __name__ == "__main__":
     Thread(target=run_flask).start()
